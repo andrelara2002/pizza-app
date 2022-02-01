3bac4ff01e705f54bbabcb8cec8b5a3d95f0ee6f
André Lara <66387919+andrelara2002@users.noreply.github.com>
2022-02-01 19:15:21 -0300

    API de ordem de pedidos

---
+++ src/services/orderApi.js
@@ -0,0 +1,56 @@
+const api = require('../services/api');
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const getCart = async () => {
+    return await AsyncStorage.getItem('cart')
+}
+
+export const deleteCart = async () => {
+    await AsyncStorage.setItem('cart', null)
+}
+
+export const createOrder = async () => {
+    const order = await getCart();
+    return await api.post('/order', { order: order })
+        .then(order => {
+            await getCart();
+            return order
+        })
+}
+
+export const getUserOrders = async (token) => {
+    return await api.post('/order', { token })
+}
+
+export const getOrderById = async (id, token) => {
+    return await api.post('/order/getbyid', { id, token })
+}
+
+export const updateOrderStatus = async (id, token, status) => {
+    return await api.put('/order/changestatus', { id, token, status })
+}
+
+export const updateOrder = async (obj) => {
+    return await api.put('/order', { obj })
+}
+
+export const cancelOrder = async (id, token) => {
+    return await api.delete('/order', { id, token })
+        .then(() => {
+            await getCart();
+            return { status: 200, message: "Order canceled" }
+        })
+        .catch(err => {
+            console.log("Error canceling order")
+            return { err, status: 500 }
+        })
+
+}
+
+export const getActiveOrder = async (token) => {
+    return await api.post('/order/activeorder', { token })
+}
+
+export const finishOrder = async (id, token) => {
+    return await api.post('/order/finishorder', { id, token })
+}
